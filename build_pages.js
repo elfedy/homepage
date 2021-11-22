fa6fec72f98a04c9c46a19ca042b79591cec2565
spikeonmars <federodriguez.88@gmail.com>
2021-11-22 13:59:36 -0300

    Basic article index

--- build_pages.js
+++ build_pages.js
@@ -5,14 +5,13 @@ let Mustache = require('mustache');
 let pagesDir = './src/pages';
 let articlesDir = './src/pages/articles';
 let articles = [
-  /*
   {
     file: 'store_url_and_friendly_redirect_in_phoenix.html',
     title: 'Storing previous urls and doing friendly redirects in phoenix',
     date: '2018-07-09',
   },
   {
-    file: 'testing_cutom_form_helpers_in_phoenix.html',
+    file: 'testing_custom_form_helpers_in_phoenix.html',
     title: 'Testing Custom Form Helpers in Phoenix',
     date: '2018-03-31',
   },
@@ -21,7 +20,6 @@ let articles = [
     title: 'An introduction to JavaScript Asynchronous Programming - Part 2 | DOM Events',
     date: '2018-02-19',
   },
-  */
   {
     file: 'asynchronous_programming_part_1.html',
     title: 'An introduction to JavaScript Asynchronous Programming - Part 1 | General Definitions and Timers',
@@ -42,7 +40,9 @@ let articles = [
 let layout =
   fs.readFileSync(pagesDir + '/layouts/index.html', 'utf8');
 
+
 // Build articles
+let indexBody = ''
 articles.forEach( article =>  {
   let pageContent = fs.readFileSync(path.join(articlesDir, article.file), 'utf8');
   let page =
@@ -57,8 +57,25 @@ articles.forEach( article =>  {
       }
     );
 
-    fs.writeFileSync(`./build/articles/${article.file}`, page, 'utf8');
+  let articlePath = `/articles/${article.file}`
+  indexBody += `
+  <li>
+    <time>${article.date}</time>
+    <h2><a href=${articlePath}>${article.title}</a></h2>
+  </li>
+  `
+
+  fs.writeFileSync(`./build/articles/${article.file}`, page, 'utf8');
 })
+
+let indexContent = `
+  <ul>
+    ${indexBody}
+  </ul>
+`;
+let index = Mustache.render(layout, {content: indexContent});
+fs.writeFileSync(`./build/index.html`, index, 'utf8');
+
 
 // Build each page inside the layout
 let dirEnts = fs.readdirSync(pagesDir, {withFileTypes: true});
