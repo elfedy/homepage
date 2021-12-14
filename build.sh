1707a3d96a22a43f9f18506ce21e339d518dac0f
spikeonmars <federodriguez.88@gmail.com>
2021-12-14 18:10:32 -0300

    Add more verbose logging to build

--- build.sh
+++ build.sh
@@ -12,18 +12,22 @@ echo starting build..
 timestart=$SECONDS
 
 # html pages
+echo building html pages...
 node build_pages.js
 
 # css
+echo building css files...
 for file in src/css/*.css
 do
   ./build_css_file.sh $file 
 done
 
+echo copying games...
 # copy games (src should just have a build)
 cp -r src/games/* build/games
 
 # copy images
+echo copying images...
 cp src/images/* build/
 
 timeend=$SECONDS
